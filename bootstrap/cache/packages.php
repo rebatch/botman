730ea95e61598bd6f558e62067fca8fd54cfb011
Roy Batchelor <roy@Roys-MacBook-Air.local>
2018-04-14 09:14:01 -0700

    Initial commit with all views

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,56 @@
+<?php return array (
+  'botman/botman' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\BotMan\\BotManServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'BotMan' => 'BotMan\\BotMan\\Facades\\BotMan',
+    ),
+  ),
+  'botman/driver-web' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Drivers\\Web\\Providers\\WebServiceProvider',
+    ),
+  ),
+  'botman/studio-addons' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Studio\\Providers\\StudioServiceProvider',
+      1 => 'BotMan\\Studio\\Providers\\RouteServiceProvider',
+    ),
+  ),
+  'botman/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'BotMan\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+);
